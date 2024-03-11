af4c88d5c7c3f9818a19a70ef748543f1c4d3133
Bhanu <bhanusngh12345@gmail.com>
2024-03-11 15:56:59 +0530

    fetch items and create items controllers created

--- src/controllers/items.controller.js
+++ src/controllers/items.controller.js
@@ -4,7 +4,6 @@ export const createItem = async(req,res) => {
     try {
         const{title,description,tag,totalWeight,originalPrice,discount,image} = req.body;
 
-        console.log(image)
         if(!title||!description||!tag||!totalWeight||!originalPrice||!discount||!image){
             return res.status(400).json({
                 success:false,
@@ -12,12 +11,40 @@ export const createItem = async(req,res) => {
             })
         }
 
-        console.log(title)
+        const uploadedData = await Item.create({
+          title,
+          description,
+          tag,
+          originalPrice,
+          discount,
+          image,
+          totalWeight
+        })
+
         return res.status(200).json({
             success:true,
-            message:image
+            message:"image is uploaded",
+            uploadedData
         })
     } catch (error) {
         console.log(error)
     }
 }
+
+export const getTotalItems = async (req, res) => {
+  try {
+    const response = await Item.find({});
+    return res.status(200).json({
+      success: true,
+      message: "Every item fetched",
+      response,
+    });
+  } catch (error) {
+    console.log(error);
+    // Handle the error appropriately, perhaps by sending an error response.
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,13 +1,9 @@
-import {v2 as cloudinary} from 'cloudinary';
-          
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_NAME, 
-  api_key: process.env.CLOUDINARY_APIKEY, 
-  api_secret: process.env.CLOUDINARY_APISECRET
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: 'dtpkznexh',
+  api_key: '516826392758339',
+  api_secret: 'wI-AtXUWgiITm94RGTKdVwAa6k0'
 });
 
-export default cloudinary
-// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//   { public_id: "olympic_flag" }, 
-//   function(error, result) {console.log(result);
-// });
+export default cloudinary;
